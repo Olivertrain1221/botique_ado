f255de0853d097c2448a7ef26d6810f620e3c1ad
Olivertrain1221 <olivetrain1996@gmail.com>
2022-05-13 23:37:16 +0100

    Added sizes to display on clothing used shell

--- bag/views.py
+++ bag/views.py
@@ -5,19 +5,15 @@ from django.shortcuts import render, redirect
 def view_bag(request):
     """ A view that renders the bag contents page """
 
-
     return render(request, 'bag/bag.html')
 
-
 def add_to_bag(request, item_id):
-    """
-    Adds to the bag
-    """
+    """ Add a quantity of the specified product to the shopping bag """
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
-    ### session is basically run so they can go back forth with the pages
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
@@ -25,3 +21,4 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+    
