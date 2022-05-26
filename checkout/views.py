d16a59cfea45325571c4f36cc9531d987c9de7ae
Olivertrain1221 <olivetrain1996@gmail.com>
2022-05-26 20:08:07 +0100

    Got payment via stripe to render

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51L3lWHIPenxIodgkpBjniAOKeiGogg9uxdpQWAimUN1wekiioiYfQJa1lVtHJFTZZWdSzAMUtacl1VxMBRBK8Vie00bcrDjCs8',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
